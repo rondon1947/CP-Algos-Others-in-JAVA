7f8e7ca763776c70b581afaae880a3a9d085f299
rondon1947 <rohan.nishant1999@gmail.com>
2020-07-19 23:10:49 +0530

    Eighth Commit

---
+++ Udemy - Data Structures Deep Dive in JAVA challenges/Reverse Merge Sort/src/rohan/nishant/Main.java
@@ -0,0 +1,59 @@
+package rohan.nishant;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+    public static void mergeSort(ArrayList<Integer> arr, int start, int end) {
+        if (start < end) {
+            int mid = (end + start) / 2;
+            mergeSort(arr, start, mid);
+            mergeSort(arr, mid + 1, end);
+            merge(arr, start, mid, end);
+        }
+    }
+    private static void merge(ArrayList<Integer> arr, int start, int mid, int end) {
+        int leftArrayLength = mid - start + 1;
+        int rightArrayLength = end - mid;
+        int[] leftArray = new int[leftArrayLength];
+        int[] rightArray = new int[rightArrayLength];
+        for (int i = 0; i < leftArrayLength; ++i)
+            leftArray[i] = arr.get(start + i);
+        for (int i = 0; i < rightArrayLength; ++i)
+            rightArray[i] = arr.get(mid + 1 + i);
+        int i = 0, j = 0, k = start;
+        while (i < leftArrayLength && j < rightArrayLength) {
+            if (leftArray[i] >= rightArray[j]) {
+                arr.set(k, leftArray[i]);
+                i++;
+            } else {
+                arr.set(k, rightArray[j]);
+                j++;
+            }
+            k++;
+        }
+        while (i < leftArrayLength) {
+            arr.set(k, leftArray[i]);
+            i++;
+            k++;
+        }
+        while (j < rightArrayLength) {
+            arr.set(k, rightArray[j]);
+            j++;
+            k++;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        ArrayList<Integer> arr = new ArrayList<>();
+        int n = sc.nextInt();
+        for (int i = 0; i < n; ++i)
+            arr.add(rd.nextInt(100));
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr.get(i) + "\t");
+        System.out.println();
+        mergeSort(arr, 0, n - 1);
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr.get(i) + "\t");
+    }
+}
---
+++ Udemy - Data Structures Deep Dive in JAVA challenges/Sort Strings using Radix Sort/src/rohan/nishant/Main.java
@@ -0,0 +1,43 @@
+package rohan.nishant;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> arr = new ArrayList<>();
+        int n = sc.nextInt();
+        for (int i = 0; i < n; ++i)
+            arr.add(sc.next());
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr.get(i) + "\t");
+        System.out.println();
+        radixSort(arr, 26, arr.get(0).length(), n);
+        for (String value : arr) {
+            System.out.print(value + "\t");
+        }
+    }
+    public static void radixSort(ArrayList<String> input, int radix, int width, int n) {
+        for (int i = 0; i < width; i++) {
+            radixSingleSort(input, i, radix, n);
+        }
+    }
+    public static void radixSingleSort(ArrayList<String> input, int position, int radix, int n) {
+        int[] countArray = new int[radix];
+        for (String value: input) {
+            countArray[getDigit(position, value, radix)]++;
+        }
+        for (int j = 1; j < radix; j++) {
+            countArray[j] += countArray[j - 1];
+        }
+        String[] temp = new String[n];
+        for (int tempIndex = n - 1; tempIndex >= 0; tempIndex--) {
+            temp[--countArray[getDigit(position, input.get(tempIndex), radix)]] = input.get(tempIndex);
+        }
+        for (int tempIndex = 0; tempIndex < n; tempIndex++) {
+            input.set(tempIndex, temp[tempIndex]);
+        }
+    }
+    public static int getDigit(int position, String value, int radix) {
+        return value.charAt(position) / (int) Math.pow(radix, position) % radix;
+    }
+}
