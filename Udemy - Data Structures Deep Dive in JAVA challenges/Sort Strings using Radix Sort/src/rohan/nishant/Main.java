195e9f347858370cc056bbb00f05249f0a4f5719
rondon1947 <rohan.nishant1999@gmail.com>
2020-07-20 07:24:16 +0530

    Ninth Commit

--- Udemy - Data Structures Deep Dive in JAVA challenges/Sort Strings using Radix Sort/src/rohan/nishant/Main.java
+++ Udemy - Data Structures Deep Dive in JAVA challenges/Sort Strings using Radix Sort/src/rohan/nishant/Main.java
@@ -17,27 +17,27 @@ public class Main {
         }
     }
     public static void radixSort(ArrayList<String> input, int radix, int width, int n) {
-        for (int i = 0; i < width; i++) {
+        for (int i = width - 1; i >= 0; i--) {
             radixSingleSort(input, i, radix, n);
         }
     }
     public static void radixSingleSort(ArrayList<String> input, int position, int radix, int n) {
         int[] countArray = new int[radix];
         for (String value: input) {
-            countArray[getDigit(position, value, radix)]++;
+            countArray[getIndex(position, value)]++;
         }
         for (int j = 1; j < radix; j++) {
             countArray[j] += countArray[j - 1];
         }
         String[] temp = new String[n];
         for (int tempIndex = n - 1; tempIndex >= 0; tempIndex--) {
-            temp[--countArray[getDigit(position, input.get(tempIndex), radix)]] = input.get(tempIndex);
+            temp[--countArray[getIndex(position, input.get(tempIndex))]] = input.get(tempIndex);
         }
         for (int tempIndex = 0; tempIndex < n; tempIndex++) {
             input.set(tempIndex, temp[tempIndex]);
         }
     }
-    public static int getDigit(int position, String value, int radix) {
-        return value.charAt(position) / (int) Math.pow(radix, position) % radix;
+    public static int getIndex(int position, String value) {
+        return value.charAt(position) - 'a';
     }
 }
