ad55e4a9eecdd3f3e2bef1fc93773dd3bc762e0f
rondon1947 <rohan.nishant1999@gmail.com>
2021-02-21 01:00:11 +0530

    Twenty-First Commit

--- Coding Blocks FAANG Interview Course/Trailing Zeroes in Factorial/src/com/company/Main.java
+++ Coding Blocks FAANG Interview Course/Trailing Zeroes in Factorial/src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt(), count = 0, div = 5;
+	    while((n/div) >= 1) {
+	        count += (n/div);
+	        div = div*5;
+        }
+	    System.out.println(count);
     }
 }
